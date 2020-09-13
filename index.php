5949ec4a6466b7ac9f53b74b7c864799d4edee63
apsofatl <71222526+apsofatl@users.noreply.github.com>
2020-09-13 12:33:18 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>Calculator</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css">
+</head>
+<header>
+    <h1>Adam's Calculator</h1>
+</header>
+
+<body class="center">
+    <input type="text" id="input" placeholder="Input equation here">
+    <button type="button" onclick="doMath()" id="inputbutton">Go</button>
+    <p id="answer"></p>
+    <p>Enter the equation into the text box. Your equation can include any of the following:$
+        Addition +, Subtraction -, Division /, Exponents ^, Multiplication *</p>
+    <script src="index.js"></script>
+</body>
+
+</html>
