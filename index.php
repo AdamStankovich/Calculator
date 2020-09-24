77fab1150761f2bf3393e3dbf79ab3d5ee9c4ba2
apsofatl <apsofatl@gmail.com>
2020-09-24 04:11:35 -0400

    Fixed undefined and injection issues

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <!doctype html>
-<html>
+<html lang="en">
 
 <head>
     <title>Calculator</title>
     <meta charset="UTF-8">
+    <meta name="author" content="Adam Stankovich">
     <link rel="stylesheet" href="index.css">
 </head>
-<header>
-    <h1>Adam's Calculator</h1>
-</header>
 
 <body class="center">
-    <input type="text" id="input" placeholder="Input equation here">
+    <header>
+        <h1>Adam's Calculator</h1>
+    </header>
+    <input type="text" id="input" pattern="[0-9+\-/^*% ]" placeholder="Input equation here">
     <button type="button" onclick="doMath()" id="inputbutton">Go</button>
     <p id="answer"></p>
-    <p>Enter the equation into the text box. Your equation can include any of the following:$
-        Addition +, Subtraction -, Division /, Exponents ^, Multiplication *</p>
+    <p>Enter the equation into the text box. Your equation can include any of the following:<br>
+        Addition +, Subtraction -, Division /, Exponents ^, Multiplication *, & Modulo %</p>
     <script src="index.js"></script>
 </body>
 
--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
 function doMath() {
-  document.getElementById("answer").innerHTML = eval(
-    document.getElementById("input").value.replace("^", "**")
-  );
+	var regex = RegExp("[0-9+-/^*%]");
+	if (regex.test(document.getElementById("input").value.replace("^", "**"))) {
+		var answer = eval(
+			document.getElementById("input").value.replace("^", "**")
+		);
+		if (answer !== undefined) {
+			document.getElementById("answer").innerHTML = answer;
+		} else {
+			document.getElementById("answer").innerHTML =
+				"Please enter an equation into the text field.";
+		}
+	} else {
+		document.getElementById("answer").innerHTML =
+			"Please enter a valid equation.";
+	}
 }
