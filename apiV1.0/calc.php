28ab9eb492319cc08efe54062bbf1e2d43aadc73
apsofatl <apsofatl@gmail.com>
2020-11-09 14:49:39 -0500

    Created access token authorization on API

--- apiV1.0/calc.php
+++ apiV1.0/calc.php
@@ -2,48 +2,61 @@
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
-    # Pull numbers/operation from request
-    $num1 = $_GET['num1'];
-    $num2 = $_GET['num2'];
-    $op = $_GET['op'];
+    # Get token from header
+    $headers = apache_request_headers();
+    $token = $headers['Authorization'];
 
-    if ($op === '+' || $op === '-' || $op === '*' || $op === '/' || $op === '**') {
-
-        $n1 = floatval($num1);
-        $n2 = floatval($num2);
-        $result = Null;
-
-        # Perform math operation and assign answer to $result
-        switch ($op) {
-            case '+':
-                $result = $n1 + $n2;
-                break;
-            case '-':
-                $result = $n1 - $n2;
-                break;
-            case '*':
-                $result = $n1 * $n2;
-                break;
-            case '/':
-                $result = $n1 / $n2;
-                break;
-            case '**':
-                $result = $n1 ** $n2;
-                break;
-        }
-
-        if ($result !== Null) {
-            # set cookie to result for mr.php api functions
-            setcookie('mr', $result);
-            echo $result;
-        }
-        # Something went terribly wrong...
-        else {
-            http_response_code(500);
-        }
-    } else {
-        http_response_code(400);
+    # validate token
+    if ($token !== 'bob') {
+        http_response_code(401);
         exit();
+    }
+
+    # if token was valid, proceed with GET request
+    else {
+        # Pull numbers/operation from request
+        $num1 = $_GET['num1'];
+        $num2 = $_GET['num2'];
+        $op = $_GET['op'];
+
+        if ($op === '+' || $op === '-' || $op === '*' || $op === '/' || $op === '**') {
+
+            $n1 = floatval($num1);
+            $n2 = floatval($num2);
+            $result = Null;
+
+            # Perform math operation and assign answer to $result
+            switch ($op) {
+                case '+':
+                    $result = $n1 + $n2;
+                    break;
+                case '-':
+                    $result = $n1 - $n2;
+                    break;
+                case '*':
+                    $result = $n1 * $n2;
+                    break;
+                case '/':
+                    $result = $n1 / $n2;
+                    break;
+                case '**':
+                    $result = $n1 ** $n2;
+                    break;
+            }
+
+            if ($result !== Null) {
+                # set cookie to result for mr.php api functions
+                setcookie('mr', $result);
+                echo $result;
+            }
+            # Something went terribly wrong...
+            else {
+                http_response_code(500);
+            }
+        } else {
+            http_response_code(400);
+            exit();
+        }
     }
 } else if ($_SERVER['REQUEST_METHOD'] == 'HEAD') {
     exit();
--- apiV1.0/mr.php
+++ apiV1.0/mr.php
@@ -1,30 +1,86 @@
 <?php
 
-# give user mr value
+# User requests memory recall GET request
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    echo ($_COOKIE['mr']);
+
+    # Get token from header
+    $headers = apache_request_headers();
+    $token = $headers['Authorization'];
+
+    # validate token
+    if ($token !== 'bob') {
+        http_response_code(401);
+        exit();
+    }
+
+    # if token was valid, return memory recall value
+    else {
+        echo ($_COOKIE['mr']);
+    }
 }
 
-# set mr value
+# User requests memory recall POST (update) request
 else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $val = $_GET['val'];
-    setcookie('mr', $val);
-}
 
-# update or set mr value if null
-else if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
-    if ($_COOKIE['mr'] !== Null) {
-        $val = $_GET['val'];
-        setcookie('mr', $val);
-    } else {
+    # Get token from header
+    $headers = apache_request_headers();
+    $token = $headers['Authorization'];
+
+    # validate token
+    if ($token !== 'bob') {
+        http_response_code(401);
+        exit();
+    }
+
+    # if token was valid, return memory recall value
+    else {
         $val = $_GET['val'];
         setcookie('mr', $val);
     }
 }
 
-# delete mr value
+# update or set mr value if null
+else if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+
+    # Get token from header
+    $headers = apache_request_headers();
+    $token = $headers['Authorization'];
+
+    # validate token
+    if ($token !== 'bob') {
+        http_response_code(401);
+        exit();
+    }
+
+    # if token was valid, proceed with PUT request
+    else {
+        if ($_COOKIE['mr'] !== Null) {
+            $val = $_GET['val'];
+            setcookie('mr', $val);
+        } else {
+            $val = $_GET['val'];
+            setcookie('mr', $val);
+        }
+    }
+}
+
+# User requests memory recall DELETE request
 else if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
-    setcookie('mr', '', time() - 3600);
+
+    # Get token from header
+    $headers = apache_request_headers();
+    $token = $headers['Authorization'];
+
+    # validate token
+    if ($token !== 'bob') {
+        http_response_code(401);
+        exit();
+    }
+
+    # if token was valid, return memory recall value
+    else {
+        setcookie('mr', '', time() - 3600);
+    }
 }
 
 # return nothing (used for header)
